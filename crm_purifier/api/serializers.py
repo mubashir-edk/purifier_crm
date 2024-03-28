b3bf69190bde11726461fa9bef6dff8869d56d26
mubashir-edk <mubashiredk03@gmail.com>
2024-03-28 14:51:38 +0530

    api update

--- crm_purifier/api/serializers.py
+++ crm_purifier/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from user_management.models import CustomUser
-from purifier.models import Employee, Customer, Test, ServiceWork, CustomerProduct, EmployeeNotification, CustomerNotification
+from purifier.models import Employee, Customer, Test, ServiceWork, CustomerProduct, EmployeeNotification, CustomerNotification, Product
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,23 +11,34 @@ class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
         exclude = ['initial_password',]
+        
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
 
 class CustomerSerializer(serializers.ModelSerializer):
+    installed_product = ProductSerializer()
     class Meta:
         model = Customer
         exclude = ['initial_password',]
 
 class TestSerializer(serializers.ModelSerializer):
+    customer_code = CustomerSerializer()
     class Meta:
         model = Test
         fields = '__all__'
 
 class ServiceWorkSerializer(serializers.ModelSerializer):
+    customer_code = CustomerSerializer()
+    product = ProductSerializer()
     class Meta:
         model = ServiceWork
         fields = '__all__'
 
 class CustomerProductSerializer(serializers.ModelSerializer):
+    customer_code = CustomerSerializer()
+    product = ProductSerializer()
     class Meta:
         model = CustomerProduct
         fields = '__all__'
